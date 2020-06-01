c5f6d68cc9af5c0ea7f63bee6d487990e70b703a
Jusan Magno Pinto da Silva <jusanmagno@gmail.com>
2020-06-01 18:09:15 -0300

    marker customizado

--- src/views/game.jsx
+++ src/views/game.jsx
@@ -44,7 +44,7 @@ const GoAr = (props) => {
                     </a-marker>
                     <a-marker preset="hiro">
 
-                        <a-entity
+                        <a-entity scale="3 3 3"
                             obj-model="obj: url(/covid.obj); 
                                         mtl: url(/covid.mlt)">
                         </a-entity>

83890ce6bf451e58d960375ad17c69bbd13cef1c
Jusan Magno Pinto da Silva <jusanmagno@gmail.com>
2020-06-01 01:11:59 -0300

    ar inside obj

--- src/views/game.jsx
+++ src/views/game.jsx
@@ -4,6 +4,7 @@ import Dimension from 'react-dimensions';
 const width = Dimension.width;
 const height = Dimension.height;
 
+
 const GoAr = (props) => {
 
     return (
@@ -20,17 +21,16 @@ const GoAr = (props) => {
                 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
                 <script src="https://aframe.io/releases/0.6.1/aframe.min.js"> </script>
                 <script src="https://jeromeetienne.github.io/AR.js/aframe/build/aframe-ar.js"> </script>
-                <script src="https://rawgit.com/donmccurdy/aframe-extras/master/dist/aframe-extras.loaders.min.js"></script>
+
             </head>
             <body style={{ margin: 0, overflow: 'hidden' }}>
                 <a-scene embedded arjs='sourceType: webcam;'>
+                    <a-assets>
+                        <a-asset-item id="carro-obj" src="../obj/carro/bugatti.obj"></a-asset-item>
+                        <a-asset-item id="carro-mtl" src="../obj/carro/bugatti.mtl"></a-asset-item>
+                    </a-assets>
                     <a-marker preset='hiro'>
-
-
-                        <a-entity
-                            obj-model="obj: url(../obj/carro/bugatti.obj);  mtl: url(../obj/carro/bugatti.mtl)">
-                            >
-                        </a-entity>
+                        <a-entity obj-model="obj: #carro-obj; mtl: #carro-mtl"></a-entity>
                     </a-marker>
                     <a-entity camera></a-entity>
                 </a-scene>

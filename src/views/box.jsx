e2ae5b0a490b982ab54f8d46ebad1dc0eec87056
Jusan Magno Pinto da Silva <jusanmagno@gmail.com>
2020-06-01 02:18:02 -0300

    box inside

--- src/views/box.jsx
+++ src/views/box.jsx
@@ -27,7 +27,7 @@ const Box = (props) => {
                 <a-scene embedded arjs='sourceType: webcam;'>
 
                     <a-marker preset='hiro'>
-                        <a-box position='0 0.5 0' material='opacity: 0.5;'></a-box>
+                        <a-box position='0 0.5 0' material='opacity: 0.5;color: blue'></a-box>
                     </a-marker>
                     <a-entity camera></a-entity>
                 </a-scene>

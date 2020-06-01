193e320cf42db611954c055e7017e44f583d43e7
Jusan Magno Pinto da Silva <jusanmagno@gmail.com>
2020-05-31 22:52:39 -0300

    atualizacao

--- src/views/game.jsx
+++ src/views/game.jsx
@@ -21,10 +21,10 @@ const GoAr = (props) => {
                 <script src="https://aframe.io/releases/0.6.1/aframe.min.js"></script>
                 <script src="https://cdn.rawgit.com/jeromeetienne/AR.js/1.5.0/aframe/build/aframe-ar.js"> </script>
             </head>
-            <body style={{margin : 0, overflow: 'hidden'}}>
+            <body style={{ margin: 0, overflow: 'hidden' }}>
                 <a-scene embedded arjs='sourceType: webcam;'>
-                    <a-marker type='barcode' value='5'>
-                        <a-box position='0 0.5 0' material='color: blue;'></a-box>
+                    <a-marker preset='hiro'>
+                        <a-box position='0 0.5 0' material='color: red;'></a-box>
                     </a-marker>
                     <a-entity camera></a-entity>
                 </a-scene>
